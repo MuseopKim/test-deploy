31794c3917d6cd2434e97c8b5604778734fa3141
ragdoll <museopkim0214@gmail.com>
2020-04-24 18:48:25 +0900

    test-commit

--- src/main/java/com/example/practiceoauth/PracticeOauthApplication.java
+++ src/main/java/com/example/practiceoauth/PracticeOauthApplication.java
@@ -3,15 +3,23 @@ package com.example.practiceoauth;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 @SpringBootApplication
-public class PracticeOauthApplication {
+public class PracticeOauthApplicatione extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(PracticeOauthApplication.class, args);
+        SpringApplication.run(PracticeOauthApplicatione.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PracticeOauthApplicatione.class);
     }
 
 }
